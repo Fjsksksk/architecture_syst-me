ebfb804fc54736a90a91e3c9c2df64e783ca035a
candice <tinouff@outlook.fr>
2024-03-24 15:03:43 +0100

    minimum sans la gestion du temps ( démarrage de l'attente avant la fin de l'execution)

--- planificateur.c
+++ planificateur.c
@@ -1,55 +1,61 @@
 #include "planificateur.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <string.h> // Pour utiliser strtok
 
+void planifier_taches(const char *commande, int delai, int iterations)
+{
+    if (delai <= 0)
+    {
+        fprintf(stderr, "Erreur : Le délai doit être supérieur à zéro.\n");
+        return;
+    }
 
-void planificateur(int nb_repetition, int temps_attente, char* cmd){
-    int i;
-    for(i=0; i<nb_repetition; i++){
-        printf("Execution de la commande %s\n", cmd);
-        int duration = temps_execution(cmd);
-        if(i != nb_repetition - 1){
-            if(duration > temps_attente){
-                sleep(temps_attente);
-            }else{
-                sleep(temps_attente - duration);
-            }
-        }else{
-            printf("Fin de la planification\n");
-        }
-        
+    time_t start_time;
+    time(&start_time); // Enregistrement du temps de départ
+
+    for (int i = 0; i < iterations; i++)
+    {
+        // Exécution de la commande
+        printf("Exécution de la commande : %s\n", commande);
+        system(commande);
+
+        // Calcul du temps écoulé depuis le début de l'exécution
+        time_t current_time;
+        time(&current_time);
+        double elapsed_time = difftime(current_time, start_time);
+
+        // Calcul du temps d'attente jusqu'à la prochaine exécution
+        int remaining_time = delai - ((int)elapsed_time % delai);
+        printf("Attente jusqu'à la prochaine exécution : %d secondes\n", remaining_time);
+        sleep(remaining_time);
     }
 }
 
-int temps_execution(char* cmd){
-    clock_t start, end;
-    double cpu_time_used;
-    start = clock();
-    system(cmd);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    return cpu_time_used;
-    
-}
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: %s <\"commande\"> <delai> <iterations>\n", argv[0]);
+        return 1;
+    }
 
-int main(int argc, char* argv[]){
-    //Demande nb de repetition
-    int nb_repetition;
-    printf("Entrez le nombre de répétition: ");
-    scanf("%d", &nb_repetition);
-    //Demande temps d'attente
-    int temps_attente;
-    printf("Entrez le temps d'attente: ");
-    scanf("%d", &temps_attente);
-    //Demande commande
-    char cmd[100];
-    printf("Entrez la commande: ");
-    scanf("%s", cmd);
+    // Récupération des arguments de la ligne de commande
+    const char *commande = strtok(argv[1], "\"");
+    int delai = atoi(argv[2]);
+    int iterations = atoi(argv[3]);
 
+    // Vérification que le délai est supérieur à zéro
+    if (delai <= 0)
+    {
+        fprintf(stderr, "Erreur : Le délai doit être supérieur à zéro.\n");
+        return 1;
+    }
 
+    // Appel de la fonction de planification des tâches
+    planifier_taches(commande, delai, iterations);
 
-    planificateur(nb_repetition, temps_attente, cmd);
     return 0;
 }
--- planificateur.h
+++ planificateur.h
@@ -1,19 +1,6 @@
 #ifndef PLANIFICATEUR_H
 #define PLANIFICATEUR_H
 
-void planificateur(int nb_repetition, int temps_attente, char* cmd);
-
-int temps_execution(char* cmd);
-
-
-
-
-
-
-
-
-
-
-
+void planifier_taches(const char *commande, int delai, int iterations);
 
 #endif // PLANIFICATEUR_H
